e5d8c0fab715aa22f1fbe9bd99249066a769533b
Mr. A <andikawahyusyaputra@gmail.com>
2023-07-27 16:21:14 +0700

    minor security fixes

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import gradio as gr
 import openai
 import os
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
-load_dotenv()
+# load_dotenv()
 
 openai.api_key = os.getenv("OPEN_AI_API_KEY")
 
